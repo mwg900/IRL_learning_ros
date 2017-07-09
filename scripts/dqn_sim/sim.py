2360c9039866cfb0f5e4e8a472f66960c8dd5224
moon <mwg900@naver.com>
2017-07-09 13:24:38 +0900

    sim fime 완성(coke can 생성)

--- scripts/dqn_sim/sim.py
+++ scripts/dqn_sim/sim.py
@@ -1,59 +1,55 @@
 #!/usr/bin/env python 
 #-*- coding: utf-8 -*- 
 
-import sys
+import os
 import rospy
+import random
+#import service messages
 from IRL_learning_ros.srv import SpawnPos
+from gazebo_msgs.srv import DeleteModel
 
-def add_two_ints_client(x, y):
-    rospy.wait_for_service('add_two_ints')
-    try:
-        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
-        resp1 = add_two_ints(x, y)
-        return resp1.sum
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-
-def usage():
-    return "%s [x y]"%sys.argv[0]
+class simulation_server:
+    def __init__(self): 
+        node_name = "sim_server" 
+        rospy.init_node(node_name)
+        s = rospy.Service('model_respawn', SpawnPos, self.model_respawn)
+        print "Ready to spwan the model."
+        
+        
+    #randomly selected spawn positon
+    def rand_pos(self):    
+        x = random.uniform(-10.0, 10.0)
+        y = random.uniform(-10.0, 10.0)
+        return x, y
+        
+        
+    #delete service 요청 함수 (to gazebo)
+    def delete_model(self, model_name):
+        rospy.wait_for_service('gazebo/delete_model')
+        d = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+        d(model_name)
+        
+    #spawn service 콜백 함수 (from spawn req)
+    def model_respawn(self, srv):
+        model_name = 'coke_can'
+        #res 요청이 들어왔을 시 실행
+        if srv.req is True:
+            #delete existing model
+            self.delete_model(model_name)
+            #System 명령어 호출
+            #"rosrun gazebo spawn_model -file <path to xml/urdf file> -urdf -model <model_name>"
+            x, y = self.rand_pos()
+            os.system("rosrun gazebo_ros spawn_model -database coke_can -gazebo -model {} -x {} -y {}".format(model_name, x, y))
+            res = 'model respawned at {:>3},{:>3}'.format(x,y)
+        else:
+            #delete model
+            # = os.system("rosservice call gazebo/delete_model '{model_name: coke_can}'")
+            self.delete_model(model_name)
+            res = 'model deleted'
+        return res
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:
-        print usage()
-        sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
-    print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
-    
-    
-
-'''
-import rospy 
-from visualization_msgs.msg import Marker
-from robot_mapping.msg import Slavepos
-#from robot_mapping.srv import Position, PositionResponse 
-import math 
- 
-#클래스 시작 
-class robot_marker: 
-    def __init__(self): 
-        node_name = "sim" 
-        rospy.init_node(node_name, anonymous=True) #노드 초기화. 노드명은 1개만 가능
-        position_sub = rospy.Subscriber("/position", Slavepos, self.callback, queue_size= 100)  #포지션 메세지 서브스크라이버 선언
-        self.marker_pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)    #Marker 메세지 사용하는 viualization_marker 토픽 퍼플리셔 설정  
-
-    def callback(self, msg):
-        pass
-        
-
- 
-if __name__ == '__main__': 
-    try: 
-        main = robot_marker()
-        rospy.spin()                    #루프 회전 
+    try:
+        main = simulation_server()
+        rospy.spin()
     except rospy.ROSInterruptException: pass
-    
-    
-    
