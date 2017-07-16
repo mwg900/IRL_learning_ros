f47871f46be10b5c8a1920f7b7bb5e5be8d5d6ca
moon <mwg900@naver.com>
2017-07-16 10:21:01 +0900

    0.0.5
    기본적인 가제보 모델 생성

--- scripts/autonomous_driving/agent_state_publisher.py
+++ scripts/autonomous_driving/agent_state_publisher.py
@@ -3,7 +3,7 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 
 class state_pub:
     def __init__(self): 
@@ -11,22 +11,26 @@ class state_pub:
         rospy.init_node(node_name)
         #ros topic 구독자 설정 및 콜백함수 정의
         laser_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback, queue_size= 100)
-        self.pub = rospy.Publisher('/state', Float32, queue_size=10)
+        self.pub = rospy.Publisher('/state', Float32MultiArray, queue_size=10)
         self.rate = rospy.Rate(10) # 10hz
-        
+        self.F = False
         
     #Laser 토픽 콜백
     def scan_callback(self, LaserScan):
         #topic 복사
         self.scan_msg = LaserScan
+        print(self.scan.ranges[100:105])
         self.F = True
         
     
-    def talker():
+    def talker(self):
         while not rospy.is_shutdown():
-            #pub.publish(avg_cost)
-            self.rate.sleep()
-    
+            if self.F is True:
+                msg = Float32MultiArray()
+                msg.data = self.scan.ranges
+                pub.publish(msg)
+            #self.rate.sleep()
+     
 if __name__ == '__main__':
     try:
         main = state_pub()
--- scripts/dqn_sim/sim.py
+++ scripts/dqn_sim/sim.py
@@ -15,13 +15,24 @@ class simulation_server:
         s = rospy.Service('model_respawn', SpawnPos, self.model_respawn)
         print ("Ready to spwan the model.")
         
-        
+
+
+        #Kobuki 생성
+        #rosrun gazebo_ros spawn_model -file `rospack find IRL_learning_ros`/urdf/kobuki_hexagons_asus_rplidar.urdf.xacro -urdf -y 1 -model rrbot1 -robot_namespace kobuki
+        kobuki_description = "`rospack find IRL_learning_ros`/urdf/kobuki_hexagons_asus_rplidar.urdf.xacro"
+        arm_description = "`rospack find irl_dual_arm`/urdf/irl_dual_arm.urdf"
+        agent_name = 'IRL_kobuki'
+        x = 0
+        y = 0
+        commend = "rosrun gazebo_ros spawn_model -unpause -urdf -file {} -model {} -x {} -y {} -Y 0 robot_namespace {}".format(kobuki_description, agent_name, x, y, agent_name)
+        #commend = "rosrun gazebo_ros spawn_model -urdf -file {} -model {} -x {} -y {} ".format(arm_description, 'arm', x, y)
+        print(commend)
+        os.system(commend)
     #randomly selected spawn positon
     def rand_pos(self):    
         x = random.uniform(-10.0, 10.0)
         y = random.uniform(-10.0, 10.0)
         return x, y
-        
         
     #delete service 요청 함수 (to gazebo)
     def delete_model(self, model_name):
@@ -36,8 +47,8 @@ class simulation_server:
         #res 요청이 들어왔을 시 실행
         self.delete_model(model_name)           # delete existing model
         x, y = self.rand_pos()                  # randomly selected spawn positon
-        os.system("rosrun gazebo_ros spawn_model -database coke_can -gazebo -model {} -x {} -y {}".format(model_name, x, y))     #System 명령어 호출 = "rosrun gazebo spawn_model -file <path to xml/urdf file> -urdf -model <model_name>"
-        return model_name, x, y
+        os.system("rosrun gazebo_ros spawn_model -database coke_can -gazebo -model {} -z 5 -x {} -y {}".format(model_name, x, y))     #System 명령어 호출 = "rosrun gazebo spawn_model -file <path to xml/urdf file> -urdf -model <model_name>"
+        return model_name, x, y 
 
 if __name__ == "__main__":
     try:
