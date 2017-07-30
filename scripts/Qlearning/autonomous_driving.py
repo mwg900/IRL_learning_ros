260c90b99d62ce5167af1550d07ee215e0b22ea0
moon <mwg900@naver.com>
2017-07-31 06:06:43 +0900

    1.5.0.0
    launch 수정 및 자율주행 모드 추가

--- scripts/Qlearning/autonomous_driving.py
+++ scripts/Qlearning/autonomous_driving.py
@@ -22,8 +22,9 @@ from argparse import Action
 
 
 #Hyper parameter
-ENVIRONMENT = rospy.get_param('/driving_train/environment', 'v0')
-MODEL_PATH = rospy.get_param('/driving_train/model_path', default = 'model')
+ENVIRONMENT = rospy.get_param('/autonomous_driving/environment', 'v1')
+MODEL_PATH = rospy.get_param('/autonomous_driving/model_path', default = 'model')
+MODEL_DATA = rospy.get_param('/autonomous_driving/model_data', default = '1620')
 
 if ENVIRONMENT == 'v0':
     INPUT_SIZE =  register.environment.v0['input_size']
@@ -54,7 +55,8 @@ class state_pub:
         self.pub = rospy.Publisher('/action', Int8, queue_size=10)
         self.rate = rospy.Rate(5) # 10hz
         self.F = False 
-        
+        self.load_epi = str(MODEL_DATA)
+        self.model_path = MODEL_PATH +"/confirmed_model/"+ENVIRONMENT+"/"+self.load_epi+"/driving15"+ENVIRONMENT+".ckpt"
         
     #Laser 토픽 콜백
     def state_callback(self, msg):
@@ -65,24 +67,39 @@ class state_pub:
 
     def talker(self):
         with tf.Session() as sess:
-            mainDQN = dqn.DQN(sess, INPUT_SIZE, OUTPUT_SIZE)    #DQN class 선언
+            mainDQN = dqn.DQN(sess, INPUT_SIZE, OUTPUT_SIZE, name = "main")    #DQN class 선언
+            #targetDQN = dqn.DQN(sess, INPUT_SIZE, OUTPUT_SIZE, name ="target")
             init = tf.global_variables_initializer()
+            saver = tf.train.Saver(max_to_keep= 5)
             sess.run(init)
             #------------------------------------------------------------------------------ 
             # Model load
             #------------------------------------------------------------------------------ 
-            if INIT_EPISODE is not 0:
-                load_epi = str(INIT_EPISODE)
-                model_name = self.model_path+"-"+load_epi
-                saver.restore(sess, model_name)            #저장된 데이터 불러오기
-                print("Model restored from {}".format(model_name))
+            model_name = self.model_path+"-"+self.load_epi
+            saver.restore(sess, model_name)            #저장된 데이터 불러오기
+            print("Model restored from {}".format(model_name))
             #------------------------------------------------------------------------------ 
-            
+            reward_sum = 0
+            reward = 0
             while not rospy.is_shutdown():                      #루프 실행
                 if self.F is True:
                     state = self.state          # 거리정보 복사
-                    action = np.argmax(mainDQN.predict(state))  #거리에 따른 액션 값 획득(로봇 행동 지령)
+                    done = self.done
+                        # Reward Policy
+                    try:
+                        if POLICY == 'autonomous_driving':
+                            reward = policy.autonomous_driving(action, done)     #reward 리턴
+                        elif POLICY == 'autonomous_driving1':
+                            reward = policy.autonomous_driving1(action, done)     #reward 리턴
+                    except:
+                        print('there is no policy') 
+                    action = np.argmax(mainDQN.predict(state, 1.0))  #거리에 따른 액션 값 획득(로봇 행동 지령)
                     self.pub.publish(action)            #액션 값 퍼블리시
+                    
+                    reward_sum += reward
+                    print("action : {:>5}, current score : {:>5}".format(action, reward_sum))   
+                    if done == True:
+                        reward_sum = 0                
                     self.rate.sleep()                   #ROS sleep
             
 if __name__ == '__main__':
